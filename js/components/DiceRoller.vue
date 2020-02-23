2186f9d832fce22d5221d354ffc99eb6604277d1
Blake Watson <blake@blakewatson.com>
2020-02-22 21:10:33 -0600

    turn off dev mode

--- js/components/DiceRoller.vue
+++ js/components/DiceRoller.vue
@@ -32,7 +32,7 @@ export default {
 
     data() {
         return {
-            devMode: true,
+            devMode: false,
             devDice: '',
             dice: [6, 6, 6, 6, 6],
             isRolling: false,
